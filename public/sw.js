c26e1617a577c582520c25e6a66b0dcd559ff099
Lucas <lucaswood@Lucas-Laptop.modem>
2025-12-19 17:04:24 +1030

    Deploy latest changes

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,7 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installed");
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener('fetch', (event) => {
+  // For now, just pass through requests
+});
